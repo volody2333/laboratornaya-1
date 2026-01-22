e9e2d9f6ebeebfc540acaf5d493237731280c691
volody2333 <volody2333@mail.ru>
2026-01-23 00:49:34 +1000

    Не закончил работу

--- main.c
+++ main.c
@@ -1,10 +1,44 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char *argv[]) {
-	double a, b, c;
-	double D, x1, x2;
-	return 0;
+ int main() {
+	
+	printf("\n--- D > 0 ---\n");
+	double a = 1.0, b = -5.0, c = 6.0;
+	double D = pow(b, 2) - 4 * a * c;
+	
+ if (D > 0) {
+		
+	double x1 = (-b + pow(D, 0.5)) / (2 * a);
+	double x2 = (-b - pow(D, 0.5)) / (2 * a);
+	
+	printf("D = %.2d\n", D);
+	printf("x1 = %.2f\n", x1);
+	printf("x2 = %.2f\n", x2);
+ }
+    
+    printf("\n--- D = 0 ---\n");
+    
+    a = 2.0; b = 4.0; c = 2.0;
+    D = pow(b, 2) - 4 * a * c;
+    
+ if (D == 0) {
+ 	
+	double x = (-b) / (2 * a);
+	printf("D = %.2f\n", D);
+	printf("x = %.2f\n", x);
 }
+
+    printf("\n--- D < 0 ---\n");
+    a = 1.0; b = 2.0; c = 5.0;
+    D = pow(b, 2) - 4 * a * c;
+ if (D < 0) {
+ 	
+    printf("D = %.2f\n", D);
+    printf("Korney net.\n");
+}
+	
+    return 0;	
+}
+
+
